1bee1d968b592626affafeec7a2a0cf195f52276
sanidhya9322 <sanipethe22@gmail.com>
2025-09-01 15:15:09 +0530

    Updated package.json and add firebaseConfig.js

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,23 +1,23 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-import { getAuth } from "firebase/auth";
-import { initializeApp } from "firebase/app";
-
 
 const firebaseConfig = {
-  apiKey: "AIzaSyABuS5QsmCeSAUz6ZWrfMeN0J_FKPDVXgY",
-  authDomain: "myamravati-market-4e7ca.firebaseapp.com",
-  projectId: "myamravati-market-4e7ca",
-  storageBucket: "myamravati-market-4e7ca.firebasestorage.app",
-  messagingSenderId: "715944765731",
-  appId: "1:715944765731:web:9947de8abf460b09a78496"
+  apiKey: "AIzaSyCcMTrPRrddN84zbsoZZtKQ068wqOYJelY",
+  authDomain: "myamravati-market-17248.firebaseapp.com",
+  projectId: "myamravati-market-17248",
+  storageBucket: "myamravati-market-17248.firebasestorage.app",
+  messagingSenderId: "390266313293",
+  appId: "1:390266313293:web:fe9ecb9eae7bcd05361a04"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
-const auth = getAuth(app);
 
-export { db, storage, auth };
+export { auth, db, storage };
