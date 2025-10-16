83a22a52e8732e09c2eab4220139afb77949c974
sanidhya9322 <sanipethe22@gmail.com>
2025-10-16 13:39:23 +0530

    Update signup.jsx

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,8 +1,10 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+// firebaseConfig.js
+
+// Ensure getApps and getApp are imported from firebase/app
+import { initializeApp, getApps, getApp } from "firebase/app"; 
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
+import { getStorage } from "firebase/storage"; 
 
 const firebaseConfig = {
   apiKey: "AIzaSyCcMTrPRrddN84zbsoZZtKQ068wqOYJelY",
@@ -14,7 +16,9 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = !getApps().length
+  ? initializeApp(firebaseConfig)
+  : getApp(); 
 const auth = getAuth(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
