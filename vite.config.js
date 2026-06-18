3809fd0779048a43e88d634b6857a7fe5fa255e7
sanidhya9322 <sanipethe22@gmail.com>
2026-06-18 19:09:21 +0530

    update changes

---
+++ vite.config.js
@@ -0,0 +1,44 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+
+export default defineConfig({
+  plugins: [
+    react(),
+
+    VitePWA({
+      registerType: "autoUpdate",
+
+      manifest: {
+        name: "MyAmravati Market",
+        short_name: "MyAmravati",
+        description:
+          "Buy and sell products locally in Amravati District",
+
+        theme_color: "#2563eb",
+        background_color: "#ffffff",
+
+        display: "standalone",
+
+        start_url: "/",
+
+        icons: [
+          {
+            src: "/logo192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/logo512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,png,svg,ico}"],
+      },
+    }),
+  ],
+});
